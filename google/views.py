e82e5e3f4f89fd1ad7785d7bde910467429b9f0b
git380 <108381952+git380@users.noreply.github.com>
2024-01-19 15:34:45 +0900

    ユーザ情報追加表示

--- google/views.py
+++ google/views.py
@@ -8,4 +8,13 @@ def toppage(request):
 
 @login_required
 def home(request):
+    # ユーザ名取得
+    print(f"Current user: {request.user.username}")
+    # 全ユーザ情報取得
+    for attr in dir(request.user):
+        try:
+            print(f"{attr}: {getattr(request.user, attr)}")
+        except AttributeError:
+            print(f"{attr}: [アクセス不可]")
+    # HTML
     return render(request, 'home.html')
